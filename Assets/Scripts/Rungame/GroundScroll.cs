d770ab0118e7ee01502df3bd1c10dd2ee7e1d562
frod2793 <tmdwhd0344@naver.com>
2022-12-01 01:29:45 +0900

    rungame player change

--- Assets/Scripts/Rungame/GroundScroll.cs
+++ Assets/Scripts/Rungame/GroundScroll.cs
@@ -24,8 +24,9 @@ public class GroundScroll : MonoBehaviour
     {
         temp =tiles[0];
     }
+    
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         if (gameUiManager.isPlay)
         {
