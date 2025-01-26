5e49bd403b278cfb9600dda94ebd07e5e9397c0a
frod2793 <tmdwhd0344@naver.com>
2025-01-26 22:11:52 +0900

    방패애니메이션 에 따른 대미지 부여

--- Assets/Scripts/Vamser_like/Weaphon/Weaphon_Shield.cs
+++ Assets/Scripts/Vamser_like/Weaphon/Weaphon_Shield.cs
@@ -6,12 +6,15 @@ namespace DogGuns_Games.vamsir
     public class Weaphon_Shield : Weaphon_base
     {
         [SerializeField]private GameObject shield;
+        [SerializeField] private Collider2D shieldCollider;
         
         bool isAnimShield; // 중복 호출 방지 플래그
         
         public override void OnEnable()
         {
             base.OnEnable();
+         //   shieldCollider = shield.GetComponent<Collider2D>();
+            shieldCollider.enabled = false;
         }
 
         public override void Weaphon_Idle()
@@ -39,7 +42,14 @@ namespace DogGuns_Games.vamsir
             shield.transform.DOLocalMove(new Vector3(0, 0, 0), 0.5f)
                 .SetEase(Ease.OutBounce)
                 .From(new Vector3(0, 1, 0))
-                .OnComplete(() => isAnimShield = false);
+                .OnUpdate(() =>
+                {
+                    shieldCollider.enabled = shield.transform.localPosition == Vector3.zero;
+                })
+                .OnComplete(() => 
+                {
+                    isAnimShield = false;
+                });
         }
     }
 }
