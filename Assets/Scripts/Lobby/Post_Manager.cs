97a0fdf9b44e9425a2ae71affa1c8f559b3ae74d
frod2793 <tmdwhd0344@naver.com>
2025-03-03 16:36:02 +0900

    우편함 시스템 고도화 작업중

--- Assets/Scripts/Lobby/Post_Manager.cs
+++ Assets/Scripts/Lobby/Post_Manager.cs
@@ -1,8 +1,27 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Post_Manager : MonoBehaviour
 {
-    [SerializeField] private GameObject MessingerPanel;
+
+    struct postmessage
+    {
+        string message;
+        string sender;
+        string date;
+    }
+    
+    [FormerlySerializedAs("MessingerPanel")] 
+    [SerializeField] private GameObject postBoxPanel;
+    [SerializeField] private GameObject postboxcontainer;
+    [SerializeField] private GameObject postboxPrefab;
+    
+    
+    // todo: 프리펩에 메시지 와 물품 아이템 코드를 저장하고 받을수 있는 클래스를 생성하여 사용 
+    
+    
+    
+    
 }
 
